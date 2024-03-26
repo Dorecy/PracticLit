ee134fc77fa8d9063a63503dd60b136cc629d006
Student S.S <hhhhh@email.com>
2024-03-26 16:48:19 +0400

    practic day one

---
+++ Windows/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PraktikaLitvinov.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PraktikaLitvinov
+{
+    public partial class MainWindow : Window
+    {
+      public class AppDbContext : DbContext
+        {
+            public DbSet<User> Users { get; set; }
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            {
+                optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Safiyanov; Integrated Security=True;Connect Timeout=30;Encrypt=False;");
+            }
+        }
+        public class User
+        {
+            public int Id { get; set; }
+
+            public string Login { get; set; }
+
+            public string Password { get; set; }
+        }
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Registration reg = new Registration();
+
+            reg.Show();
+
+            this.Hide();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void Vhod_Click(object sender, RoutedEventArgs e)
+        {
+            var login = Login.Text;
+            var password = Password.Text;
+            var context = new AppDbContext();
+
+            var user = context.Users.SingleOrDefault(x => x.Login == login && x.Password == password);
+            if (user is null) 
+            {
+                MessageBox.Show("Неправильный логин или пароль!");
+                return;
+            }
+            MessageBox.Show("Вы успешно вошли в аккаунт!");
+        }
+    }
+}
---
+++ Windows/Registration.xaml.cs
@@ -0,0 +1,66 @@
+﻿using PraktikaLitvinov.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PraktikaLitvinov
+{
+    /// <summary>
+    /// Логика взаимодействия для Registration.xaml
+    /// </summary>
+    public partial class Registration : Window
+    {
+        public Registration()
+        {
+            InitializeComponent();
+        }
+
+        private void Vxod_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow avt = new MainWindow();
+
+            avt.Show();
+
+            this.Hide();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void Reg_Click(object sender, RoutedEventArgs e)
+        {
+            var login = loginreg.Text;
+            var pass = passwordreg.Text;
+            var email = emailreg.Text;
+
+            var context = new AppDbContext();
+
+            var user_exists = context.Users.FirstOrDefault( x => x.Login == login);
+            if (user_exists is not null) 
+            {
+                MessageBox.Show("Такой пользователь уже в клубе крутышек");
+                return;
+            }
+            var user = new User { Login = login, Password = pass};
+            context.Users.Add(user);
+            context.SaveChanges();
+            MainWindow avt = new MainWindow();
+
+            avt.Show();
+
+            this.Hide();
+        }
+    }
+}
